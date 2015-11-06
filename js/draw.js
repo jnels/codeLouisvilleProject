8f4523610a58fecf888db807cfc707901be0bfd2
jnels <s0ckeyeus@yahoo.com>
2015-11-05 20:48:11 -0500

    Initial commit. Initialized Bootstrap. Added modal to poetry page and revised draw.js

--- js/draw.js
+++ js/draw.js
@@ -10,30 +10,72 @@ var canvas = new fabric.Canvas("poetry-canvas", {
 
 canvas.freeDrawingBrush.width = 15;
 
-function displayPoem (userText) {
+function displayPoem (userText, topPosition) {
 	var printText = new fabric.Text(userText, {  
-		top: 10,
-		fontSize: 16,
+		top: 15,
+		left: 15,
+		fontSize: 18,
 		fontFamily: "Merriweather"
 	});
 	
 	canvas.add(printText);
 	
 	//Hide form, show "new" button
-	$("#form").hide();
-	$("#poetry-canvas").css("display", "block");
-	$("#new-canvas-button").css("display", "block");
-	$("#save-canvas-button").css("display", "block");
-	$(".controls").css("display", "block");
+	$(".poem").hide();
+	$(".controls").show();
+		//.css("display", "block");
 }
 
 //Allow user to add text to canvas
 $("#submit-button").click(function(){
-	$("#poetry-input").hide();
-	//Put value of textarea into paragraph
+	$("#create-new-poem").html("Create new found poem");
+	$("#save-canvas-button").show();
+	$("input").removeAttr("checked");
+	//Put value of textarea into canvas
   $userText = $("#input-text").val();
+	$("#input-text").val("");
+//	countCharacters($userText);
 	displayPoem($userText)
 });
+
+
+	var joinedArray = [];
+	var textArray = [];
+
+//Experimental
+function countCharacters(val) {
+//	textArray = [];
+	var length = $userText.length;
+	var charBreak = 15;
+	var j = 0;
+
+	
+	for (i = 0; i < length; i++) {
+		var next = i + 1;
+		var character = $userText.substring(i, next);
+		textArray.push(character);
+	}
+	
+	for (i = 0; i < textArray.length; i++) {
+		var newLineArray = [];
+		if (textArray[i] === " " && j < 5) {
+			j += 1;
+		} else if (textArray[i] === " " && j >= 5) {
+			newLineArray.push(textArray[i]);
+			joinedArray = newLineArray.join();
+			j = 0;
+			displayPoem(joinedArray, 15);
+			
+		} else {
+			j += 1;
+			console.log(j);
+		}
+	}
+	
+	//console.log("final " + textArray);
+	//displayPoem(joinedArray)
+}
+
 
 //Color selection
 $(".controls").on("click", "li", function(){
@@ -54,13 +96,13 @@ $("#save-canvas-button").click(function(){
 });
 
 //Clear fields to allow new text. Also clear canvas.
-$("#new-canvas-button").click(function(){
-		canvas.clear().renderAll();
-		$("#poetry-input").show();
-		$("#input-text").val("");
-		$("#new-canvas-button").hide();
-		$("#save-canvas-button").hide();
-	});
+//$("#new-canvas-button").click(function(){
+//		canvas.clear().renderAll();
+//		$("#poetry-input").show();
+//		$("#input-text").val("");
+//		$("#new-canvas-button").hide();
+//		$("#save-canvas-button").hide();
+//	});
 
 
 //Poetry
@@ -95,13 +137,16 @@ $("input[type=radio]").click(function () {
 	
 	if (poem === "#create") {
 		$("#poetry-input").show(); //.css("display", "block");
-		//$("#submit-button").show();
-	} else {	
+	} else {
+		$("#poetry-input").hide();
 		$(title).show();
 		$(poem).show();
 	}
 	canvas.clear().renderAll();
 });
+
+
+
 
 
 
