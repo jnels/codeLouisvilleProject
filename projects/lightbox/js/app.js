4e7c460afb4761f2c4f2f3d7f46bd1c770b719d8
jnels <s0ckeyeus@yahoo.com>
2015-11-18 21:05:49 -0500

    Revamped lightbox to improve movement from image to image

--- projects/lightbox/js/app.js
+++ projects/lightbox/js/app.js
@@ -1,12 +1,13 @@
 
 var $overlay = $("#overlay");
 var $overlayContent = $("#overlayContent");
-
 var $image = $("<img>");
 var $caption = $("<p></p>");
-
-var i = 0;
+//var i = 0;
+var imageLocation;
 var nextImage;
+var picList = [];
+var picIndex;
 
 //Add image to overlay
 $overlayContent.append($image);
@@ -14,55 +15,61 @@ $overlayContent.append($image);
 //Add caption to overlay
 $overlayContent.append($caption);
 
+//Loads image src into array
+$("li img").each(function(i) {
+//	var position = $(this).attr("href");
+	var position = $(this).attr("src");
+	picList.push(position);
+});
+
 //Capture click event on link to image
 $('#imageGallery a').click(function(event){
 		
   event.preventDefault();
-	//Show overlay
-  $overlay.show();
-	
-  var imageLocation = $(this).attr("href");
-	displayImage(imageLocation);
-});  
-
-function displayImage(location) {
-	$image.attr("src", location);
 	
 	//Get child's alt attribute and set caption
   var captionText = $(this).children("img").attr("alt");
   $caption.text(captionText);
+	
+	//Show overlay
+  $overlay.show();
+	
+	//Pull href attribute
+	imageLocation = $(this).attr("href");
+	
+	//Loop to find value in picList array
+	for (var i = 0; i < picList.length; i++) {
+		if (picList[i] === imageLocation) {
+			picIndex = i;
+			displayImage(imageLocation);
+		} 
+	}
+});  
+
+function displayImage(location) {
+	$image.attr("src", location);
 }
 
 $("#next").click(function() {
-	
-	nextImage = $("li").find("a").eq(i).attr("href");
-	
-	if (nextImage === undefined) {
-		nextImage = $("#imageGallery li:first-child").attr("href");
-		console.log(nextImage);
-		i=1;
-		i++;
+	if (picIndex === picList.length - 1) {
+		picIndex = 0;
+	} else { 
+		picIndex++
 	}
-		i++;
-		displayImage(nextImage);
+	
+	imageLocation = picList[picIndex];
+	displayImage(imageLocation);
 });
 
 $("#previous").click(function() {
-	nextImage = $("li").find("a").eq(i).attr("href");
-	console.log(nextImage);
-	
-	var futurePic = $("li").find("a").eq(i-1).attr("href");
-	console.log("X " + futurePic);
-	
-	if (futurePic === undefined) {
-		nextImage = $("#imageGallery li:last-child").attr("href");
-		i=1;
-		i--;
+	if (picIndex === 0) {
+		picIndex = picList.length - 1;
+	} else { 
+		picIndex--
 	}
 	
-	i--;
-	displayImage(nextImage);
-//	};
+	imageLocation = picList[picIndex];
+	displayImage(imageLocation);
 });
 
 $(".close-overlay").click(function(){
